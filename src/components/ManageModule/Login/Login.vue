ac976537dd121df5b0b9bda8e3ba720de6d80667
BigFacer <grhnavergiveup@163.com>
2019-12-03 17:57:58 +0800

    Download plugins (ElementUi、Vuex、Axios)

---
+++ src/components/ManageModule/Login/Login.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="login">
+
+            <div class="head">
+              <div class="sd"><span class="sdsd">welcome</span></div>
+              <div class="name">
+                 <p class=" nameDiv"><i class="el-icon-user-solid"></i></p>
+              </div>
+            </div>
+    </div>
+
+</template>
+<style>
+  #login{
+     width: 100%;
+    height: 100%;
+    background: #000000;
+    /*padding-top: 200px;*/
+  }
+  .sd{
+    padding-top: 200px;
+    color: #ededed;
+    font-style:  italic;
+    font-size: 50px;
+
+  }
+  .sdsd{
+    display: inline-block;
+    animation: headfont 2s;
+  }
+  @keyframes headfont {
+    from{
+      margin-top: -200px;
+      color: #000;
+    }
+    to{
+      margin-top: 0;
+      color: #ededed;
+    }
+  }
+  .nameDiv{
+    width: 200px;
+    border-bottom: 2px solid #ededed;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router/index'
+import ElementUI from 'element-ui'
 
-Vue.config.productionTip = false;
-
+Vue.config.productionTip = false
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
   template: '<App/>'
-});
+})
